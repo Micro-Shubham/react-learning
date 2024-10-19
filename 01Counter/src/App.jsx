86734dd33314a8e57eebf15c52a4d4e2831516bd
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-10-19 13:33:13 +0530

    creating counter and understanding hooks

--- 01Counter/src/App.jsx
+++ 01Counter/src/App.jsx
@@ -1,9 +1,23 @@
-
+import { useState } from 'react';
+import './App.css'
 function App() {
-
+let [counter , setCounter]  = useState(0)
+//  let counter =5;
+ const addValue =() => {
+  // console.log("clicked",counter)
+  setCounter(counter +1)
+ }
+ const removeValue =() =>{
+  setCounter(counter-1);
+ }
   return (
     <>
-    
+    <h1>Hello there</h1>
+    <h2>Counter value :{counter}</h2>
+    <br/>
+    <button onClick={addValue}>Increase :{counter}</button>
+    <button onClick={removeValue}>Decrease :{counter}</button>
+    <p>footer:{counter}</p>
    </>
   )
 }
